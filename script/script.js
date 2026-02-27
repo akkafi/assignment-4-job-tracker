6c36b6254e7f1d61b6d33f5990d157bc76caa422
akkafi <abdullacst11@gmail.com>
2026-02-27 16:03:49 +0600

    solved

--- script/script.js
+++ script/script.js
@@ -9,11 +9,8 @@ const allSectionContainer = document.getElementById('all-section-container');
 const interviewSectionContainer = document.getElementById('interview-section-container');
 const rejectedSectionContainer = document.getElementById('rejected-section-container');
 const emptyState = document.getElementById('empty-state');
-// console.log(allSectionContainer, interviewSectionContainer, rejectedSectionContainer);
 
 function currentTab(currentTab){
-    // console.log(button)
-
     currentButton = currentTab;
     const allTabs = ['all', 'interview', 'rejected'];
     for(const tab of allTabs){
@@ -33,7 +30,6 @@ function currentTab(currentTab){
         section.classList.add('hidden')
     }
 
-    // emptyState.classList.add('hidden');
     emptyState.classList.add("hidden");
     if(currentTab === 'all'){
         allSectionContainer.classList.remove('hidden');
@@ -59,36 +55,30 @@ const countTotal =document.getElementById('count-total');
 const countInterview =document.getElementById('count-interview');
 const countReject =document.getElementById('count-reject');
 const countAvailableJobs = document.getElementById('available-jobs');
-// countTotal.innerText = allSectionContainer.children.length
 
 currentTab(currentButton);
 
 // All Job Container
-
 document.getElementById('all-job-container')
 .addEventListener('click', function(event){
-    // console.log(event.target);
     const clickElement = event.target;
     const card = clickElement.closest('.job-card');
     const parentCard = card.parentNode;
     const statusBtn = card.querySelector('.status-btn');
 
     if(clickElement.classList.contains('interview')){
-        // console.log('interview clicked');
         statusBtn.innerText = 'Interview';
         statusBtn.style.color = '#08e62d';
         statusBtn.style.border = '2px solid #08e62d';
         interviewSectionContainer.appendChild(card);
          updateStatus()
     } else if(clickElement.classList.contains('rejected')){
-        // console.log('rejected clicked');
         statusBtn.innerText = 'Rejected';
         statusBtn.style.color = 'red';
         statusBtn.style.border = '2px solid red';
         rejectedSectionContainer.appendChild(card);
          updateStatus()
     } else if(clickElement.classList.contains('delete')){
-        // console.log('delete clicked');
         parentCard.removeChild(card);
          updateStatus();
     }
